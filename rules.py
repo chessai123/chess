1ccc33890e30c14513d2337d9cb75f63d30469fc
agfagerbakk <garrifagerbakk@hotmail.com>
2019-04-03 11:19:07 +0200

    added new workstructure

---
+++ rules.py
@@ -0,0 +1,11 @@
+import chess
+import board
+
+class Rules()
+    def __init__
+    
+    "Game or win condition for continuation of the game"
+    def game_con()
+    
+    
+    def legal_moves():
---
+++ search.py
@@ -0,0 +1,15 @@
+import chess
+import board
+import rules
+import math
+
+class Search():
+    
+    def minmax(curdepth, board,  )
+        if curdepth == targetdepth:
+            return eval[value]
+        
+        if (maxplay):
+            return max
+        else:
+            return min
