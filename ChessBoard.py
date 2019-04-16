b6c3cb2ddb8a6ebe3f6e1d118d4f17aa80ea6258
agfagerbakk <garrifagerbakk@hotmail.com>
2019-04-16 19:49:24 +0200

    updated config file

--- ChessBoard.py
+++ ChessBoard.py
@@ -3,6 +3,7 @@ import sys
 import chess
 import math
 import fenparser
+import config as cfg
 
 screenW=400
 screenH=400
@@ -14,22 +15,6 @@ board_length = math.sqrt(board_size)
 column_letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 square_size = int(screenH / 8)
 
-brown_square_img    = "images/brown_square.png"
-white_square_img    = "images/white_square.png"
-cyanid_square_img   = "images/cyan_square.png"
-
-black_pawn_img      = "images/Chess_tile_pd.png"
-white_pawn_img      = "images/Chess_tile_pl.png"
-black_bishop_img    = "images/Chess_tile_bd.png"
-white_bishop_img    = "images/Chess_tile_bl.png"
-black_king_img      = "images/Chess_tile_kd.png"
-white_king_img      = "images/Chess_tile_kl.png"
-black_horse_img     = "images/Chess_tile_nd.png"
-white_horse_img     = "images/Chess_tile_nl.png"
-black_queen_img     = "images/Chess_tile_qd.png"
-white_queen_img     = "images/Chess_tile_ql.png"
-black_tower_img     = "images/Chess_tile_rd.png"
-white_tower_imp     = "images/Chess_tile_rl.png"
 
 class chessb:
     def __init__(self):
@@ -43,34 +28,34 @@ class chessb:
         self.to_position = None
 
     def LoadImages(self):
-        self.white_block = pygame.image.load(white_square_img)
-        self.brown_block = pygame.image.load(brown_square_img)
-        self.highlight_block = pygame.image.load(cyanid_square_img)
+        self.white_block = pygame.image.load(cfg.white_square_img)
+        self.brown_block = pygame.image.load(cfg.brown_square_img)
+        self.highlight_block = pygame.image.load(cfg.cyanid_square_img)
         self.highlight_block = pygame.transform.scale(self.highlight_block, (square_size,square_size))
 
-        self.black_pawn = pygame.image.load(black_pawn_img)
+        self.black_pawn = pygame.image.load(cfg.black_pawn_img)
         self.black_pawn = pygame.transform.scale(self.black_pawn, (square_size,square_size))
-        self.white_pawn = pygame.image.load(white_pawn_img)
+        self.white_pawn = pygame.image.load(cfg.white_pawn_img)
         self.white_pawn = pygame.transform.scale(self.white_pawn, (square_size,square_size))     
-        self.black_bishop = pygame.image.load(black_bishop_img)
+        self.black_bishop = pygame.image.load(cfg.black_bishop_img)
         self.black_bishop = pygame.transform.scale(self.black_bishop, (square_size,square_size))
-        self.white_bishop = pygame.image.load(white_bishop_img)
+        self.white_bishop = pygame.image.load(cfg.white_bishop_img)
         self.white_bishop = pygame.transform.scale(self.white_bishop, (square_size,square_size)) 
-        self.black_king = pygame.image.load(black_king_img)
+        self.black_king = pygame.image.load(cfg.black_king_img)
         self.black_king = pygame.transform.scale(self.black_king, (square_size,square_size))
-        self.white_king = pygame.image.load(white_king_img)
+        self.white_king = pygame.image.load(cfg.white_king_img)
         self.white_king = pygame.transform.scale(self.white_king, (square_size,square_size)) 
-        self.black_horse = pygame.image.load(black_horse_img)
+        self.black_horse = pygame.image.load(cfg.black_horse_img)
         self.black_horse = pygame.transform.scale(self.black_horse, (square_size,square_size))
-        self.white_horse = pygame.image.load(white_horse_img)
+        self.white_horse = pygame.image.load(cfg.white_horse_img)
         self.white_horse = pygame.transform.scale(self.white_horse, (square_size,square_size)) 
-        self.black_queen = pygame.image.load(black_queen_img)
+        self.black_queen = pygame.image.load(cfg.black_queen_img)
         self.black_queen = pygame.transform.scale(self.black_queen, (square_size,square_size))
-        self.white_queen = pygame.image.load(white_queen_img)
+        self.white_queen = pygame.image.load(cfg.white_queen_img)
         self.white_queen = pygame.transform.scale(self.white_queen, (square_size,square_size)) 
-        self.black_tower = pygame.image.load(black_tower_img)
+        self.black_tower = pygame.image.load(cfg.black_tower_img)
         self.black_tower = pygame.transform.scale(self.black_tower, (square_size,square_size))
-        self.white_tower = pygame.image.load(white_tower_imp)
+        self.white_tower = pygame.image.load(cfg.white_tower_imp)
         self.white_tower = pygame.transform.scale(self.white_tower, (square_size,square_size)) 
 
     def convert_to_chess_square(self, x, y):
@@ -131,121 +116,18 @@ class chessb:
         self.draw_pieces(fen)
         pygame.display.update()
 
-<<<<<<< HEAD
     def parse_fen(self):
         return fenparser.FenParser(self.board.fen()).parse()
 
     def check_if_legal(self):
         Nf3 = chess.Move(from_square=self.from_position, to_square=self.to_position)
         print(Nf3)
-=======
-    def convert_to_board_pos(self, x, y):
-        row = board_size - math.floor(y/square_size) 
-        column = math.floor(x/square_size)
-        column = column_letter[column]
-        pos = column + str(row)
-        return pos
-
-    def convert_to_chess_board_pos(self, x, y):
-        col = 0
-        for i in column_letter:
-            if i == x:
-                break
-            col += 1
-        row = board_size - int(y)
-        return (col, row)
-
-    def convert_to_chess_squares(self, x, y):
-        counter = 0
-        for i in range(board_size):
-            for j in range(board_size):
-                if (i, j) == (y, x):
-                    #print("counter:")
-                    print("counter" ,counter, ((x+1)*(y+1))-1)
-                    return counter
-                counter += 1
-        return counter        
-
-    def is_pawn_promotion(self, move):
-        print(move)
-        first_pos = move[:2]
-        second_pos = move[2:]
-        fen = self.parse_fen()
-
-        first = self.convert_to_chess_board_pos(first_pos[0], first_pos[1]) 
-        first_col = first[0]
-        first_row = first[1]
-
-        second = self.convert_to_chess_board_pos(second_pos[0], second_pos[1])
-        second_col = second[0]
-        second_row = second[1]
-
-        # check for white pawn
-        if fen[first_row][first_col] == "P" and second_row == 0:
-            from_pos = self.convert_to_chess_squares(first_row, first_col)
-            to_pos = self.convert_to_chess_squares(second_row, second_col)
-            print(first_pos, first_col, first_row)
-            print("\n")
-            print("hello", from_pos, to_pos)
-            Nf3 = chess.Move(from_square=from_pos, to_square=to_pos, promotion=chess.QUEEN)
-            self.board.push(Nf3) 
-            self.draw()
-
-        # check for black pawn
-        if fen[first_row][first_col] == "p" and second_row == 7:
-            from_pos = self.convert_to_chess_squares(first_col, first_row)
-            to_pos = self.convert_to_chess_squares(second_col, second_row)
-            Nf3 = chess.Move(from_square=from_pos, to_square=to_pos, promotion=chess.QUEEN)
-            self.board.push(Nf3) 
-            self.draw()
-
-        """
-        fen = self.parse_fen()
-        pos = move[0]
-        first_col = 0
-        for i in column_letter:
-            if i == pos:
-                break
-            first_col += 1
-        first_row = int(move[1]) -1
-
-        pos = move[2]
-        to_col = 0
-        for i in column_letter:
-            if i == pos:
-                break
-            to_col += 1
-        to_row = int(move[3]) -1
-
-        print(move)
-
-        if fen[first_row][first_col] == "P" and move[3] == "8":
-            board_pos_from = self.convert_to_chess(first_col, first_row)
-            board_pos_to = self.convert_to_chess(to_col, to_row)
-            Nf3 = chess.Move(to_square=board_pos_from, from_square=board_pos_to, promotion=chess.QUEEN)
-            print(Nf3)
-            self.board.push(Nf3)
-            self.draw()
-        elif fen[first_row][first_col] and move[3] == "1":
-            board_pos_from = self.convert_to_chess(first_col, first_row)
-            board_pos_to = self.convert_to_chess(to_col, to_row)
-            Nf3 = chess.Move(from_square=board_pos_from, to_square=board_pos_to, promotion=chess.QUEEN)
-            self.board.push(Nf3)
-            self.draw()
-            """
-            
-
-    def is_legal_move(self, move):
-        Nf3 = chess.Move.from_uci(move)
-        self.is_pawn_promotion(move)
->>>>>>> bd91cde6704ecd71585df832b0361520e584e1b2
         if Nf3 in self.board.legal_moves:
             print("legal")
             self.board.push(Nf3) 
         else:
             print("not a legal")
             return False
-<<<<<<< HEAD
 
     def move_piece(self, square):
         if self.to_position == None and self.from_position != None:
@@ -261,10 +143,6 @@ class chessb:
             return
 
     def run_game(self):
-=======
-            
-    def game_loop(self):
->>>>>>> bd91cde6704ecd71585df832b0361520e584e1b2
         pygame.event.set_blocked(pygame.MOUSEMOTION)
         self.draw()
         while True:
@@ -275,26 +153,8 @@ class chessb:
                         pos = pygame.mouse.get_pos()
                         square = self.convert_to_chess_square(pos[0], pos[1])
                         print(square)
-<<<<<<< HEAD
                         self.move_piece(square)
                         self.draw()
-=======
-                        if self.to_position == None:
-                            pass
-
-                        # checks from and to pos & checks for legality of said pos/move
-                        if self.from_position != None and self.to_position == None:
-                        
-                            board_pos = self.convert_to_board_pos(pos[0], pos[1])
-                            self.to_position = board_pos
-                            move = self.from_position + self.to_position
-                            status = self.is_legal_move(move)
-                            if status == False:
-                                print("not a legal move")
-
-                            if self.from_position == None:
-                                pass
->>>>>>> bd91cde6704ecd71585df832b0361520e584e1b2
                 if event.type == pygame.QUIT:
                     sys.exit()
 
---
+++ config.py
@@ -0,0 +1,53 @@
+# import pygame
+
+# pygame.init()
+# square_size = 50
+brown_square_img = "images/brown_square.png"
+white_square_img = "images/white_square.png"
+cyanid_square_img = "images/cyan_square.png"
+
+black_pawn_img = "images/Chess_tile_pd.png"
+white_pawn_img = "images/Chess_tile_pl.png"
+black_bishop_img = "images/Chess_tile_bd.png"
+white_bishop_img = "images/Chess_tile_bl.png"
+black_king_img = "images/Chess_tile_kd.png"
+white_king_img = "images/Chess_tile_kl.png"
+black_horse_img = "images/Chess_tile_nd.png"
+white_horse_img = "images/Chess_tile_nl.png"
+black_queen_img = "images/Chess_tile_qd.png"
+white_queen_img = "images/Chess_tile_ql.png"
+black_tower_img = "images/Chess_tile_rd.png"
+white_tower_imp = "images/Chess_tile_rl.png"
+
+# def LoadImages():
+#     white_square = pygame.image.load(white_square_img)
+#     white_square = pygame.transform(white_square,(square_size, square_size))
+#     brown_square = pygame.image.load(brown_square_img)
+#     brown_square = pygame.transform(brown_square,(square_size, square_size))
+#     #highlight_block = pygame.image.load(cyanid_square_img)
+#     #highlight_block = pygame.transform.scale(highlight_block, (square_size,square_size))
+
+#     black_pawn = pygame.image.load(black_pawn_img)
+#     black_pawn = pygame.transform.scale(black_pawn, (square_size,square_size))
+#     white_pawn = pygame.image.load(white_pawn_img)
+#     white_pawn = pygame.transform.scale(white_pawn, (square_size,square_size))     
+#     black_bishop = pygame.image.load(black_bishop_img)
+#     black_bishop = pygame.transform.scale(black_bishop, (square_size,square_size))
+#     white_bishop = pygame.image.load(white_bishop_img)
+#     white_bishop = pygame.transform.scale(white_bishop, (square_size,square_size)) 
+#     black_king = pygame.image.load(black_king_img)
+#     black_king = pygame.transform.scale(black_king, (square_size,square_size))
+#     white_king = pygame.image.load(white_king_img)
+#     white_king = pygame.transform.scale(white_king, (square_size,square_size)) 
+#     black_horse = pygame.image.load(black_horse_img)
+#     black_horse = pygame.transform.scale(black_horse, (square_size,square_size))
+#     white_horse = pygame.image.load(white_horse_img)
+#     white_horse = pygame.transform.scale(white_horse, (square_size,square_size)) 
+#     black_queen = pygame.image.load(black_queen_img)
+#     black_queen = pygame.transform.scale(black_queen, (square_size,square_size))
+#     white_queen = pygame.image.load(white_queen_img)
+#     white_queen = pygame.transform.scale(white_queen, (square_size,square_size)) 
+#     black_tower = pygame.image.load(black_tower_img)
+#     black_tower = pygame.transform.scale(black_tower, (square_size,square_size))
+#     white_tower = pygame.image.load(white_tower_imp)
+#     white_tower = pygame.transform.scale(white_tower, (square_size,square_size))
