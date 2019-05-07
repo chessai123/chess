8b22244d6c19a917d81bdcaf437794431a0e32f0
jacjoh <jjo122@post.uit.no>
2019-05-07 19:09:35 +0200

    finished calculating the score

--- evaluation.py
+++ evaluation.py
@@ -14,58 +14,6 @@ def make_move(board):
     board.push(legal_move_list[0])
     return board
 
-#the fen string is inverted, so needs to take the length of the board and [x for x in board.legal_moves]
-#subtrakt with the row number to get the right pos in the fen
-# def get_row_and_col(self, string):   
-#     col1 = 0
-#     for i in letters:
-#         if i == string[0]:
-#             break
-#         col1 += 1
-
-#     row1 = 8-int(string[1]) 
-
-#     col2 = 0
-#     for i in letters:
-#         if i == string[2]:
-#             break
-#         col2 += 1
-
-#     row2 = 8-int(string[3])
-
-#     return [col1, row1, col2, row2]
-    
-#     def evalute(self, board):
-#         pass
-
-# def evaluate_pos_score(self, move, board, player):
-#     fen = parse_fen(board)
-#     moves = self.get_row_and_col(str(move))
-    
-#     from_col    = moves[0]
-#     from_row    = moves[1]
-#     to_col      = moves[2]
-#     to_row      = moves[3]
-
-#     #get the value for the given piece
-#     piece = fen[from_row][from_col].capitalize()
-#     piece_value = config.piece[piece]
-        
-#     #get score for the position
-#     pst_pos = (config.board_length * to_row) + to_col
-#     position_score = config.pst[piece]
-#     pst_value = position_score[pst_pos]
-    
-#     to_pos = fen[to_row][to_col]
-    
-#     if to_pos != " ":
-#         to_pos_score = config.piece[to_pos]
-#         return to_pos_score + pst_value
-#     else:
-#         return pst_value
-
-
-
 def calculate_score_for_piece(piece, lowercase, row, col):
     piece = piece.upper()
     pst_pos = (config.board_length * row) + col
@@ -161,7 +109,8 @@ def min_max(depth, node, player):
     
     return max_score
 
-
+def AI_make_move():
+    pass
 
     #initial call minimax(depth, origin, True)
     
