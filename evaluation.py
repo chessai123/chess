6cea3d92e1e8935cfbab887665c21c3f7cfbe241
jacjoh <jjo122@post.uit.no>
2019-05-09 13:58:18 +0200

    working on the search algorithm

--- evaluation.py
+++ evaluation.py
@@ -2,30 +2,35 @@ import chess
 import config
 import fenparser
 import math
-import sys 
+import sys
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 
 def make_move(board):
-    #tree = Node(2, 1, 0, board)
-    print("Board score: {}".format(evaluate_board_score(board)))
-    legal_move_list = [x for x in board.legal_moves]
-    board.push(legal_move_list[0])
+    tree = Node(2, 1, 0, board)
+    move = min_max(2, tree, 1)
+    print(move)
+    # legal_move_list = [x for x in board.legal_moves]
+    board.push(move[1])
     return board
+
 
 def calculate_score_for_piece(piece, lowercase, row, col):
     piece = piece.upper()
     pst_pos = (config.board_length * row) + col
     position_scores = config.pst[piece]
-    pst_value = position_scores[pst_pos]
+    position_scores = position_scores[::-1]
 
     if lowercase:
-        piece_value = -config.piece[piece]
-    else:
         piece_value = config.piece[piece]
-
+        pst_value = position_scores[pst_pos]
+    else:
+        piece_value = -config.piece[piece]
+        pst_value = -position_scores[pst_pos]
+    print("piece value: {} position value: {}, sum: {}".format(piece_value, pst_value, piece_value+pst_value))
     return pst_value + piece_value
+
 
 def calculate_score_for_position(row, col, fen):
     piece = fen[row][col]
@@ -56,30 +61,28 @@ def calculate_score_for_position(row, col, fen):
         return calculate_score_for_piece(piece, True, row, col)
     else:
         return 0
-    
+
 
 def evaluate_board_score(board):
-    #loop through the board and calculate the score
+    # loop through the board and calculate the score
     fen = parse_fen(board)
-
     score = 0
-
     for row in range(len(fen)):
         for col in range(len(fen[row])):
             score += calculate_score_for_position(row, col, fen)
-
+            #print("this is the score: {}, row and col: {}{} this is the fen string: \n{}".format(score, row, col, fen))
     return score
-
 
 def parse_fen(board):
     return fenparser.FenParser(board.fen()).parse()
 
+
 class Node(object):
-    def __init__(self, depth, playernum, score, board):
+    def __init__(self, depth, playernum, move, board):
         self.depth = depth
         self.playernum = playernum
-        self.score = score
         self.board = board
+        self.move = move
         self.children = []
         self.generate_children()
 
@@ -88,29 +91,39 @@ class Node(object):
         if self.depth >= 0:
             for x in legal_moves:
                 self.board.push(x)
-                self.children.append(Node(self.depth-1, -self.playernum, -self.score, self.board))
+                self.children.append(Node(self.depth-1, -self.playernum, x, self.board))
                 self.board.pop()
 
-def min_max(depth, node, player):   
-    if depth == 0 or len(node.children) == 0:
-        return evaluate_board_score()
 
-    max_score = sys.maxsize * -player
+def min_max(depth, node, player):
+    if player > 0:
+        max_score = [-sys.maxsize, None]
+    else:
+        max_score = [sys.maxsize, None]
 
-    for i in range(len(node)):
-        child = node.children[i]
-        score = min_max(depth-1, child, -player)
-        if( player > 0 ):
-            if score > max_score:
-                max_score = score
+    if depth == 0:
+        score = evaluate_board_score(node.board)
+        return [score, node.move]
+
+    for child in node.children:
+        evaluation = min_max(depth-1, child, -player)
+        print(evaluation, max_score)
+        if (player > 0):
+            if(evaluation[0] > max_score[0]):
+                if(node.move != 0):
+                    max_score = [evaluation[0], node.move]
+                else:
+                    max_score = [evaluation[0], evaluation[1]]
         else:
-            if score < max_score:
-                max_score = score
-    
+            if(evaluation[0] < max_score[0]):
+                if(node.move != 0):
+                    max_score = [evaluation[0], node.move]
+                else:
+                    max_score = [evaluation[0], evaluation[1]]
     return max_score
 
 def AI_make_move():
     pass
 
-    #initial call minimax(depth, origin, True)
+    # initial call minimax(depth, origin, True)
     
